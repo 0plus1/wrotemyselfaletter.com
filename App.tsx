7bdda15a2a5451b06a5451eace39c63e4199cd3d
Davide <davide.0plus1@protonmail.com>
2022-07-11 23:59:25 +1000

    Add custom fonts

--- App.tsx
+++ App.tsx
@@ -3,10 +3,26 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 import HomeScreen from './screens/Home';
 import { default as theme } from './theme.json';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  GochiHand_400Regular 
+} from '@expo-google-fonts/gochi-hand';
 
-export default () => (
-  <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
-    <HomeScreen />
-    <StatusBar style="auto" />
-  </ApplicationProvider>
-);
+export default () => {
+  let [fontsLoaded] = useFonts({
+    GochiHand_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+
+    return (
+      <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+        <HomeScreen />
+        <StatusBar style="auto" />
+      </ApplicationProvider>
+    );
+  }
+}
