5b799b71cf9c91d9d416eb9ff2d1f77505c74949
Davide <davide.0plus1@protonmail.com>
2022-07-13 23:11:35 +1000

    Install jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "react-native",
+  setupFilesAfterEnv: [
+      "@testing-library/jest-native/extend-expect"
+  ],
+  testPathIgnorePatterns: [
+    "<rootDir>/__tests__/common/",
+  ],
+  transformIgnorePatterns: ["node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"],
+};
