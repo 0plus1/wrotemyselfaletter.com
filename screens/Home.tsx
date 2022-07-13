936e0e464a5f39d8865a176986031fd91b5a5f76
Davide <davide.0plus1@protonmail.com>
2022-07-13 22:19:49 +1000

    Home component

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,34 +1,100 @@
-import { StyleSheet } from 'react-native';
-import { Layout, Input, Text } from '@ui-kitten/components';
+import { View, StyleSheet } from 'react-native';
+import { Button, Layout, Input, Text } from '@ui-kitten/components';
+import { useForm, Controller } from 'react-hook-form'
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import Captcha from '../components/Captcha';
+import Datepicker from '../components/Datepicker';
+import FormHeader from '../components/FormHeader';
 
-const Home = () => (
-  <Layout style={styles.container}>
-    <Text>culone</Text>
-    <Input
-      style={styles.input}
-      size='small'
-      placeholder='Small'
-    />
+dayjs.extend(relativeTime);
 
-    <Input
-      style={styles.input}
-      size='medium'
-      placeholder='Medium'
-    />
+const Home = ({ navigation }) => {
+  const { control, handleSubmit, watch, formState: { errors } } = useForm({
+    defaultValues: {
+      date: dayjs().add(1, 'year').toDate(),
+      email: '',
+    },
+  });
 
-    <Input
-      style={styles.input}
-      size='large'
-      placeholder='Large'
-    />
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
-    <Input
-      multiline={true}
-      textStyle={{ minHeight: 64 }}
-      placeholder='Multiline'
-    />
-  </Layout>
-);
+  return (
+    <Layout style={styles.container}>
+      <FormHeader />
+      <View style={styles.formContainer}>
+        <Controller
+          name='letter'
+          rules={{ required: true }}
+          control={control}
+          render={({ field, fieldState: { error } }) => (
+            <Input
+              {...field}
+              status={error ? 'danger' : 'primary'}
+              multiline={true}
+              style={styles.input}
+              textStyle={{ minHeight: 200 }}
+              placeholder='Dear...'
+            />
+          )}
+        />
+
+        <Controller
+          name='date'
+          control={control}
+          render={({ field: {onChange, onBlur, value, name }, fieldState: { error } }) => {
+            return (
+              <Datepicker
+                {...{ onChange, onBlur, value, name }}
+                date={value}
+                error={error}
+                setDate={(date) => { onChange(date); }}
+              />
+            );
+           }}
+        />
+
+        <Controller
+          name='email'
+          rules={{ required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i }}
+          control={control}
+          render={({ field, fieldState: { error }  }) => (
+            <Input
+              {...field}
+              status={error ? 'danger' : 'primary'}
+              style={styles.input}
+              size='large'
+              placeholder='Your e-mail'
+            />
+          )}
+        />
+
+        <Captcha onVerify={(token, ekey) => console.log(token, ekey)} />
+        
+        <Button onPress={handleSubmit(onSubmit)}>
+          Send
+        </Button>
+      </View>
+      <Text category='c1' style={{ textAlign: 'center'}}>
+        After confirming your email address, you  will receive a letter on the {dayjs(watch('date')).format('DD MMMM YYYY')}.
+        By sending your letter, you agree to the
+        {' '}
+        <Text onPress={() => navigation.navigate('Terms') } category='c1' style={styles.textLink}>
+          terms of service
+        </Text>
+        {' '}
+        and
+        {' '}
+        <Text onPress={() => navigation.navigate('Terms') } category='c1' style={styles.textLink}>
+          privacy policy
+        </Text>
+        .
+      </Text>
+    </Layout>
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -36,9 +102,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  input: {
-    marginVertical: 2,
+  formContainer: {
+    width: '100%',
+    padding: '1%',
   },
+  input: {
+    marginBottom: 10,
+  },
+  textLink: {
+    textDecorationLine: 'underline',
+  }
 });
 
 export default Home;
