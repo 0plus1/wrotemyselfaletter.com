9d468a0313d801485a0184c0a177c8fee82056b3
Davide <davide.0plus1@protonmail.com>
2022-07-21 11:37:34 +1000

    Only generate json reports

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
   ],
   transformIgnorePatterns: ["node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"],
   collectCoverage: true,
+  coverageReporters: ["json"],
   collectCoverageFrom: [
     "**/*.{js,jsx}",
     "!**/coverage/**",
