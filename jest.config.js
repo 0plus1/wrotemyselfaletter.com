468248a095ca38c8a2e63154bc21209782650c06
Davide <davide.0plus1@protonmail.com>
2022-07-21 11:05:01 +1000

    Properly config test coverage

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,12 @@ module.exports = {
     "<rootDir>/__tests__/common/",
   ],
   transformIgnorePatterns: ["node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"],
-  coverageReporters: ["cobertura"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{js,jsx}",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+    "!**/babel.config.js",
+    "!**/jest.setup.js"
+  ]
 };
